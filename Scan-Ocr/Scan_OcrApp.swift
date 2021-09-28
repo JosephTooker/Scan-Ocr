abe07c2dbdbd5466743badea07b57a9c24e948db
Joseph Tooker <tookerjt@gmail.com>
2021-09-28 17:22:19 -0400

    Initial Commit

---
+++ Scan-Ocr/Scan_OcrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scan_OcrApp.swift
+//  Scan-Ocr
+//
+//  Created by Joseph Tooker on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Scan_OcrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
